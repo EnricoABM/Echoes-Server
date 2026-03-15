82636ca64bba517830199cf17d5a82bca0c8ccfd
Avraham <souzaavraham@proton.me>
2026-03-15 16:34:40 -0300

    refactor: separate javascript from html

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,65 @@
+(function(){
+  const loginBtn = document.querySelector('#login-form > button[type=submit]');
+  const verifyBtn = document.querySelector('#mfa-form > button[type=submit]');
+  const loginForm = document.getElementById("login-form");
+  const mfaForm = document.getElementById("mfa-form");
+  let userEmail;
+
+  async function login(email, password) {
+    const res = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, password })
+    });
+    return res.ok;
+  }
+
+  async function verifyCode(email, code) {
+    const res = await fetch("/api/auth/login/2fa", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email,
+        code,
+      })
+    });
+    return res.ok;
+  }
+
+  loginBtn.addEventListener('click', async evt => {
+    try {
+      evt.preventDefault();
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      const res = await login(email, password);
+      if (res) {
+        userEmail = email;
+        loginForm.classList.add("hidden");
+        mfaForm.classList.remove("hidden");
+      } else
+        alert('Email ou senha inválidos');
+    } catch(err) {
+      console.error(err);
+      alert('Um erro ocorreu');
+    }
+  })
+
+  verifyBtn.addEventListener('click', async evt => {
+    try {
+      evt.preventDefault();
+      const code = document.getElementById("code").value;
+      const res = await verifyCode(email, code);
+      if (res)
+        window.location.href = '/dashboard';
+      else
+        alert("Código inválido ou expirado");
+    } catch(err) {
+      console.error(err);
+      alert('Um erro ocorreu');
+    }
+  })
+})()
