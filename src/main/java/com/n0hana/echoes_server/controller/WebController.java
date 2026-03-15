51a9c187094b8672a017bc180783200905857979
Avraham <souzaavraham@proton.me>
2026-03-15 20:44:58 -0300

    feat: página de perfil

--- src/main/java/com/n0hana/echoes_server/controller/WebController.java
+++ src/main/java/com/n0hana/echoes_server/controller/WebController.java
@@ -1,9 +1,12 @@
 package com.n0hana.echoes_server.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.n0hana.echoes_server.model.User;
 
 @Controller
 public class WebController {
@@ -26,5 +29,17 @@ public class WebController {
         }
 
         return "dashboard";
+    }
+
+    @GetMapping("/dashboard/profile")
+    public String profile(Model model, @CookieValue(value = "access_token", required = false) String token) {
+        if (token == null || token.isEmpty()) {
+            return "redirect:/";
+        }
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        model.addAttribute("user", user);
+
+        return "profile";
     }
 }
