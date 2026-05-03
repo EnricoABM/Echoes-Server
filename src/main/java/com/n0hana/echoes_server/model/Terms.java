b550fb951bb29f8e527043f08837c7077fc38d51
Avraham <souzaavraham@proton.me>
2026-05-03 00:16:56 -0300

    feat: tipo de documentação do termo

--- src/main/java/com/n0hana/echoes_server/model/Terms.java
+++ src/main/java/com/n0hana/echoes_server/model/Terms.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -28,6 +30,9 @@ public class Terms {
 
   @CreationTimestamp
   private Instant timestamp;
+
+  @Enumerated(EnumType.STRING)
+  private DocumentType type;
 
   private boolean active;
 }
