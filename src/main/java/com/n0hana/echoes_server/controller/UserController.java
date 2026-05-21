d99dd07c0fef74ab4975f3e6e332f710757ded29
Enrico <enricodeabm@hotmail.com>
2026-05-21 16:15:55 -0300

    fix: retorno da role

--- src/main/java/com/n0hana/echoes_server/controller/UserController.java
+++ src/main/java/com/n0hana/echoes_server/controller/UserController.java
@@ -27,7 +27,8 @@ public class UserController {
 
         return ResponseEntity.ok(new UserInfoResponseDTO(
             user.getName(),
-            user.getEmail()
+            user.getEmail(),
+            user.getRole().toString()
         ));
     }
     
--- src/main/java/com/n0hana/echoes_server/dto/UserInfoResponseDTO.java
+++ src/main/java/com/n0hana/echoes_server/dto/UserInfoResponseDTO.java
@@ -2,7 +2,8 @@ package com.n0hana.echoes_server.dto;
 
 public record UserInfoResponseDTO(
     String name,
-    String email
+    String email,
+    String role
 ) {
     
 }
