1021a9e9f1ba010b0b920d80462006ec0ed6953d
Avraham <souzaavraham@proton.me>
2026-05-24 00:17:22 -0300

    feat: endpoint para consulta de logs para administradores

--- src/main/java/com/n0hana/echoes_server/service/logs/AuditLogService.java
+++ src/main/java/com/n0hana/echoes_server/service/logs/AuditLogService.java
@@ -1,5 +1,7 @@
 package com.n0hana.echoes_server.service.logs;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +19,9 @@ public class AuditLogService {
     @Async
     public void register(AuditLog log) {
         repository.save(log);
+    }
+
+    public Page<AuditLog> getLogs(Pageable pageable) {
+        return repository.findAll(pageable);
     }
 }
